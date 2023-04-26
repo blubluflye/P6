24a24319f2676915b513f13df06f04542aec339c
Sleepy Grizzly <nicolas.bessonnet96@orange.com>
2023-04-27 00:49:26 +0200

    Spring initializr files

---
+++ payMyBuddy/src/test/java/com/p6/payMyBuddy/PayMyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.p6.payMyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayMyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
